ba517238d8716ce3d2f6ba1ce7c1b13618933c29
Sharif Bhuiyan <106225926+phc-sbhuiyan@users.noreply.github.com>
2025-04-02 15:44:07 -0700

    Update pineapple_app.py

--- pineapple_app.py
+++ pineapple_app.py
@@ -25,13 +25,13 @@ def add_logo():
     )
     
     #hiding footer by streamlit
-    hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
-    st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+    footer="
+        <style> your css code put here</style>
+        <div class='footer'>
+        <p>the word you want to tell<a style='display:block;text-align:center;' 
+        href='https://www.streamlit.io' target='_blank'>your email address put here</a></p>
+        </div>"
+    st.markdown(footer, unsafe_allow_html=True) 
 
 def initialize_pinecone():
         api_key = st.secrets["PINECONE_API_KEY"]

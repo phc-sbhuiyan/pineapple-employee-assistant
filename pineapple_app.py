335e3568b1855b6fdadd3df6f5d55666bced3427
Sharif Bhuiyan <106225926+phc-sbhuiyan@users.noreply.github.com>
2025-04-02 18:20:02 -0700

    Update pineapple_app.py

--- pineapple_app.py
+++ pineapple_app.py
@@ -89,6 +89,7 @@ def main(assistant):
     if user_query:
         progress_text = "Operation in progress, Please wait..."
         progressBar = st.progress(0, text=progress_text)
+        percent_complete = 0
         while True:
             time.sleep(0.01)
             progressBar.progress(percent_complete + 1, text=progress_text)
